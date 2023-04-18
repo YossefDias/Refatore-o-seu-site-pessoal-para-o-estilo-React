b586624ad335a0d0f721cddd79c22a2bdef83f72
José Dias <josediaspe@gmail.com>
2023-04-18 20:48:13 -0300

    adicionei foto na capa do portfolio e criei a pasta contato

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/H/Header';
 import Main from './components/S/Main';
 import Portfolio from './components/Portfolio/Portfolio/Portfolio';
 import Footer from './components/F/Footer';
+// import Contato from './components/Contato/Contato'
 import {BrowserRouter, Route, Routes, Link} from "react-router-dom";
 
 function App() {
---
+++ src/components/Contato/Contato.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Contato.css";
+
+<>
+<main>
+<form onsubmit="event.preventDefault()">
+    <fieldset>
+             <label for="input-nome">Nome</label>
+             <input type="text" name="input-nome" id="input-nome" required minlength="2"/>            
+    </fieldset>
+    
+    <fieldset>
+     <label for="input-email">Email</label>
+     <input type="email" name="input-email" id="input-email" required/>
+    </fieldset>
+
+    <fieldset>
+        <label for="input-telefone">Telefone</label>
+        <input type="tel" name="input-tel" id="input-tel" placeholder="(99) 99999-9999" required pattern="^\(\d{2}\) \d{5}-\d{4}$" maxlength="15"/>
+    </fieldset>
+    <fieldset>
+        <label for="input-msg">Mensagem</label>
+        <textarea name="input-msg" id="input-msg" cols="30" rows="10"></textarea>
+        
+        <center>
+            <input type="submit" onclick="enviarFormulario()" value="ENVIAR" class="button" id="submit-contact"/>        
+        </center>
+    </fieldset>
+    
+</form>
+    </main>
+
+</>
+
+export default Contato 
